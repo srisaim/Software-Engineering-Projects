eb9dd8441b64e01a9d8c8b944b9e3b37ad3282cf
Sai M. Valluru <117846071+srisaim@users.noreply.github.com>
2023-12-26 12:09:43 -0500

    Delete Multi-Class Design Advanced directory

--- Multi-Class Design Advanced/WebFilmz/src/main/java/cs410/webfilmz/ILikeFilm.java
+++
@@ -1,9 +0,0 @@
-package cs410.webfilmz;
-
-/* Express preferences about films (directors and genres, specifically)
- */
-public interface ILikeFilm {
-    // Do we like the given director or genre?
-    public boolean isLikedDirector(String director);
-    public boolean isLikedGenre(String genre);
-}
